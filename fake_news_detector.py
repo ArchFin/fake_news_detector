86afbb39b697d5186f42e2d775dd98cf89a8616f
areilly14 <mr.aidan.reilly@gmail.com>
2024-08-26 13:12:15 +0100

    Vectorisation of training and test data

--- fake_news_detector.py
+++ fake_news_detector.py
@@ -22,6 +22,8 @@ def clean_text(text):
     text = re.sub('\w*\d\w*', '', text)
     return text
 
+
+
 #Read csv files
 fake_df = pd.read_csv("Data\Fake.csv")
 true_df = pd.read_csv("Data\True.csv")
@@ -41,3 +43,12 @@ news_df = news_df.drop(["subject", "title", "date"], axis = 1)
 
 #clean data
 news_df["text"] = news_df["text"].apply(clean_text)
+
+x = news_df["text"]
+y = news_df["class"]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.25)
+
+vectorisation = TfidfVectorizer()
+x_v_train = vectorisation.fit_transform(x_train)
+x_v_test = vectorisation.transform(x_test)
