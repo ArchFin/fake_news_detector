e5a4abdfc665503d20836254dd3fcdb08d8f7112
areilly14 <mr.aidan.reilly@gmail.com>
2024-08-26 12:13:07 +0100

    Uploading ML and data libraries

---
+++ fake_news_detector.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+
