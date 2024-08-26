5011a9aa48917b58b3d4d963c60ca0a4de7d5f43
areilly14 <mr.aidan.reilly@gmail.com>
2024-08-26 13:56:11 +0100

    Adding option to input your own news article

--- fake_news_detector.py
+++ fake_news_detector.py
@@ -23,6 +23,29 @@ def clean_text(text):
     return text
 
 
+def detect_fake_news(news_text):
+    testing_news = { "text" : [news_text] }
+    new_news_df = pd.DataFrame(testing_news)
+    new_news_df["text"] = new_news_df["text"].apply(clean_text)
+    new_x_test = new_news_df["text"]
+
+    #vectorisation
+    new_x_v_test = vectorisation.transform(new_x_test)
+
+    pred_lr = lr.predict(new_x_v_test)
+    pred_dt  = dt.predict(new_x_v_test)
+
+    return print("\n\nLR Prediction: {} \nDT Predicton: {}".format(label(pred_lr[0]), label(pred_dt[0])))
+
+def label(n):
+    if n==0:
+        return "Fake news"
+    elif n==1:
+        return "Real news"
+    else:
+        raise "error"
+
+
 
 #Read csv files
 fake_df = pd.read_csv("Data\Fake.csv")
@@ -70,3 +93,6 @@ dt.fit(x_v_train,y_train)
 pred_dt  = dt.predict(x_v_test)
 print(dt.score(x_v_test, y_test))
 print(classification_report(y_test,pred_dt))
+
+news_input  = input("Please put in news text, cheers")
+detect_fake_news(news_input)
